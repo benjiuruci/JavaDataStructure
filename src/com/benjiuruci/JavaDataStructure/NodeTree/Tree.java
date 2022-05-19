dfdc3ba06a29048bf4260bf7343f186c40109d7e
29906 <2990617822@qq.com>
2022-05-19 15:32:15 +0800

    二叉树

---
+++ src/com/benjiuruci/JavaDataStructure/NodeTree/Tree.java
@@ -0,0 +1,21 @@
+package com.benjiuruci.JavaDataStructure.NodeTree;
+
+/**
+ * @Author: qinxianzhong
+ * @Date: 2022/5/18 23:02
+ */
+public class Tree {
+
+    public AutoPartsNode autoPartsNode;
+
+    public void insert(AutoParts autoParts){
+        AutoParts current = autoParts;
+        AutoPartsNode autoPartsNode = new AutoPartsNode(current);
+
+        while (true){
+            if (current.getSize() < )
+        }
+
+    }
+
+}
---
+++ src/com/benjiuruci/JavaDataStructure/NodeTree/TreeMain.java
@@ -0,0 +1,12 @@
+package com.benjiuruci.JavaDataStructure.NodeTree;
+
+/**
+ * @Author: qinxianzhong
+ * @Date: 2022/5/18 23:10
+ */
+public class TreeMain {
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+        tree.insert(new AutoParts(120,"法拉利","法拉利","钢化玻璃"));
+    }
+}
